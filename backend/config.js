fa5ed81ebaf0efb04e473f84249440d86b2a7e88
Alexandra Balo <alexandrabalo@yahoo.com>
2023-10-28 22:03:55 +0300

    connected to the database

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,2 @@
 export const PORT = 5555
+export const mongoDBURL = 'mongodb+srv://root:root@cluster0.wgkrio2.mongodb.net/books-collection?retryWrites=true&w=majority'
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
 import express from "express"
-import { PORT } from "./config.js"
+import { PORT, mongoDBURL } from "./config.js"
+import mongoose from "mongoose"
 
 const app = express()
 
-app.listen(PORT, () => {
-    console.log(`App is listening on port ${PORT}`)
+app.get('/',(req,res) => {
+    console.log(req)
+    return res.status(234).send("Welcome")
+})
+
+
+
+mongoose.connect(mongoDBURL).then(() => {
+    console.log('Connected to the database')
+    app.listen(PORT, () => {
+        console.log(`App is listening on port ${PORT}`)
+    })
+    
+}).catch((error) => {
+    console.log(error)
 })
