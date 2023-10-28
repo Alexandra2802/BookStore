2359b2f501304ed2d2e0bf43d9b5529fbf0db572
Alexandra Balo <alexandrabalo@yahoo.com>
2023-10-28 22:55:05 +0300

    endpoint for updating a book by id

--- backend/index.js
+++ backend/index.js
@@ -67,4 +67,26 @@ mongoose.connect(mongoDBURL).then(() => {
     
 }).catch((error) => {
     console.log(error)
+    res.status(500).send({message: error.message})
+})
+
+//update a book by id
+app.put('/books/:id', async (req,res) => {
+    try {
+        if(!req.body.title || !req.body.author || !req.body.publishYear){
+            return res.status(400).send({message: 'Send all required fields'})
+        }
+
+        const {id} = req.params
+        const result = await Book.findByIdAndUpdate(id,req.body)
+        
+        if(!result){
+            return res.status(404).json({message: 'Book not found'})
+        }
+
+        return res.status(200).send({message:'Book updated successfully'})
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({message: error.message})
+    }
 })
