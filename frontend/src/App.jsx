af624a40539ed38a92115f2f250c1fa507fa61fb
Alexandra Balo <alexandrabalo@yahoo.com>
2023-11-06 16:22:44 +0200

    defined routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import ShowBook from './pages/ShowBook'
+import DeleteBook from './pages/DeleteBook'
+import CreateBook from './pages/CreateBook'
+import EditBook from './pages/EditBook'
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
+    <Routes>
+      <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/books/create' element={<CreateBook></CreateBook>}></Route>
+      <Route path='books/delete/:id' element={<DeleteBook></DeleteBook>}></Route>
+      <Route path='books/edit/:id' element={<EditBook></EditBook>}></Route>
+      <Route path='books/details/:id' element={<ShowBook></ShowBook>}></Route>
+    </Routes>
   )
 }
 
